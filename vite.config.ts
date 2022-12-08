a72087ca8e137d5cdaea09400aa7f343687dd309
anakon73 <nazarkinah73@gmail.com>
2022-12-08 20:11:51 +0200

    build: set up alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
+import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Components({
-      dts: true, // enabled by default if `typescript` is installed
-    }),
     vue(),
+    Components({
+      /* options */
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
