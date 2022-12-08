8feeba588281a925b7a0aaf47d91a007c5ea4e59
anakon73 <nazarkinah73@gmail.com>
2022-12-08 20:08:26 +0200

    chore: added auto import(unplugin-vue-components)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    Components({
+      dts: true, // enabled by default if `typescript` is installed
+    }),
+    vue(),
+  ],
 })
