c216503ac42fac07442b2923da33959f6f3da3f6
anakon73 <nazarkinah73@gmail.com>
2022-12-12 17:16:09 +0200

    feat(basecheckbox): create BaseCheckbox components

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+type Props = {
+  modelValue: boolean
+  label: string
+}
+const { modelValue, label } = defineProps<Props>()
+const emits = defineEmits(['update:modelValue'])
+
+const checked = (event: Event) => {
+  if (event.target instanceof HTMLInputElement) {
+    const {
+      target: { value },
+    } = event
+    emits('update:modelValue', value)
+  }
+}
+</script>
+
+<template>
+  <input type="checkbox" v-model="modelValue" @change="checked" />
+  <label v-if="label">{{ label }}</label>
+</template>
