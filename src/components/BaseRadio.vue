866ff76b4aa062dc0873865f91ec0dc648f43621
anakon73 <nazarkinah73@gmail.com>
2022-12-13 14:07:11 +0200

    feat(components/baseradio): create baseradio component

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type Props = {
+  label: string
+  modelValue: string | number
+  value: string | number
+}
+
+const { label, modelValue, value } = defineProps<Props>()
+
+const emits = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="flex items-center">
+    <input
+      type="radio"
+      :checked="modelValue === value"
+      :value="value"
+      @change="$emit('update:modelValue', value)"
+      v-bind="$attrs"
+    />
+    <label class="text-gray-500 text-xs ml-1" v-if="label">{{ label }}</label>
+  </div>
+</template>
