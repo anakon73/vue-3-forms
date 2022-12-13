ccddb0bcf339b815887cef28222b2d3be668357f
anakon73 <nazarkinah73@gmail.com>
2022-12-13 22:11:45 +0200

    feat(view/simpleform): add title and button for submit

--- src/view/SimpleForm.vue
+++ src/view/SimpleForm.vue
@@ -28,6 +28,9 @@ const petOptions = [
 </script>
 
 <template>
+  <h1 class="mb-4 text-center text-4xl font-bold text-slate-600">
+    Create an event
+  </h1>
   <div class="mb-4">
     <BaseSelect
       label="Category"
@@ -36,16 +39,20 @@ const petOptions = [
     />
   </div>
   <div class="mb-4">
-    <h3 class="text-[19px] font-bold mb-5">Name & describe your event</h3>
+    <h3 class="text-[19px] font-bold mb-5 text-slate-600">
+      Name & describe your event
+    </h3>
     <BaseInput class="mb-4" v-model="event.title" label="Title" type="text" />
     <BaseInput v-model="event.description" label="Description" type="text" />
   </div>
   <div class="mb-4">
-    <h3 class="text-[19px] font-bold mb-5">Where is your event?</h3>
+    <h3 class="text-[19px] font-bold mb-5 text-slate-600">
+      Where is your event?
+    </h3>
     <BaseInput v-model="event.location" label="Location" type="text" />
   </div>
   <div class="mb-4">
-    <h3 class="text-[19px] font-bold mb-5">Extras</h3>
+    <h3 class="text-[19px] font-bold mb-5 text-slate-600">Extras</h3>
     <BaseCheckbox
       class="mb-2"
       label="Catering"
@@ -53,13 +60,21 @@ const petOptions = [
     />
     <BaseCheckbox label="Live music" v-model="event.extras.music" />
   </div>
-  <div>
-    <h3 class="text-[19px] font-bold mb-5">Are pets allowed?</h3>
+  <div class="mb-6">
+    <h3 class="text-[19px] font-bold mb-5 text-slate-600">Are pets allowed?</h3>
     <BaseRadioGroup
       v-model="event.pets"
       name="pets"
       :options="petOptions"
       vertical
     />
+  </div>
+  <div class="text-center">
+    <button
+      type="submit"
+      class="transition-all duration-[400ms] py py-3 px-6 rounded-full font-bold text-white bg-green-400 hover:bg-green-500 hover:scale-105"
+    >
+      Submit
+    </button>
   </div>
 </template>
